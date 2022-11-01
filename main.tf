fe79e0c2d401546b3e7902455d1bc6b47c2d9917
Deniss Lapcenko <deniss.lapcenko@bidstack.com>
2022-11-01 11:15:58 +0800

    Convert module into list of healthchecks

--- main.tf
+++ main.tf
@@ -1,19 +1,23 @@
 resource "aws_route53_health_check" "check" {
-  fqdn              = var.fqdn
-  port              = var.port
-  type              = var.type
-  resource_path     = var.resource_path
-  failure_threshold = var.failure_threshold
-  request_interval  = var.request_interval
-  search_string     = var.search_string
+  for_each = var.sites
+
+  fqdn              = each.key
+  port              = each.value.port
+  type              = each.value.type
+  resource_path     = each.value.resource_path
+  failure_threshold = each.value.failure_threshold
+  request_interval  = each.value.request_interval
+  search_string     = each.value.search_string
 
   tags = {
-    Name = var.name
+    Name = each.key
   }
 }
 
 resource "aws_cloudwatch_metric_alarm" "alarm" {
-  alarm_name          = "${var.name}"
+  for_each = var.sites
+
+  alarm_name          = each.key
   namespace           = "AWS/Route53"
   metric_name         = "HealthCheckStatus"
   comparison_operator = "LessThanThreshold"
@@ -24,10 +28,10 @@ resource "aws_cloudwatch_metric_alarm" "alarm" {
   unit                = "None"
 
   dimensions = {
-    HealthCheckId = aws_route53_health_check.check.id
+    HealthCheckId = aws_route53_health_check.check[each.key].id
   }
 
-  alarm_description         = "This metric monitors ${var.name} whether the service endpoint is down or not."
+  alarm_description         = "This metric monitors ${each.key} whether the service endpoint is up or not."
   ok_actions             = [var.sns_topic]
   alarm_actions             = [var.sns_topic]
   insufficient_data_actions = [var.sns_topic]
--- variables.tf
+++ variables.tf
@@ -1,40 +1,14 @@
-variable "name" {
-  default     = false
-  description = "The name of the check"
-}
-
-variable "fqdn" {
-  default     = false
-  description = "The fully qualified domain name of the endpoint to be checked"
-}
-
-variable "port" {
-  default     = false
-  description = "The port of the endpoint to be checked"
-}
-
-variable "type" {
-  description = "The protocol to use when performing health checks. Valid values are HTTP, HTTPS, HTTP_STR_MATCH, HTTPS_STR_MATCH, TCP, CALCULATED and CLOUDWATCH_METRIC"
-  default = "HTTPS"
-}
-
-variable "search_string" {
-  default = ""
-}
-
-variable "resource_path" {
-  default     = false
-  description = "The path that you want Amazon Route 53 to request when performing health checks."
-}
-
-variable "failure_threshold" {
-  description = "The number of consecutive health checks that an endpoint must pass or fail."
-}
-
-variable "request_interval" {
-  description = "The number of seconds between the time that Amazon Route 53 gets a response from your endpoint and the time that it sends the next health-check request."
-}
-
 variable "sns_topic" {
   description = "SNS topic where publish alert"
+}
+
+variable "sites" {
+  type = map(object({
+    port = optional(number, 443)
+    type = optional(string, "HTTPS")
+    resource_path = optional(string, "/")
+    failure_threshold = optional(number, 2)
+    request_interval = optional(number, 30)
+    search_string = optional(string, "")
+  }))
 }
